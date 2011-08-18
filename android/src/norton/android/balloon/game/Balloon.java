c73843517d3fe0c4376fa4cc2f5dfa3ad868e1f2
Linus Norton <linusnorton@gmail.com>
2011-08-18 22:00:52 +0100

    Refactored the geometry

--- android/src/norton/android/balloon/game/Balloon.java
+++ android/src/norton/android/balloon/game/Balloon.java
@@ -3,13 +3,10 @@ package norton.android.balloon.game;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import norton.android.util.geometry.Particle;
 import norton.android.util.graphics.Drawable;
 
-public class Balloon implements Drawable {
-    private int x;
-    private int y;
-    private float dx;
-    private float dy;
+public class Balloon extends Particle implements Drawable {
     private int width;
     private int height;
     private Paint paint;
@@ -18,10 +15,7 @@ public class Balloon implements Drawable {
      * Create Balloon 
      */
     public Balloon(int width, int height) {
-        x = 0;
-        y = 0;
-        dx = 0;
-        dy = 0;
+        super();
         
         paint = new Paint();
         paint.setColor(Color.RED);
@@ -35,16 +29,6 @@ public class Balloon implements Drawable {
      */
     @Override
     public void onDraw(Canvas canvas) {
-        canvas.drawRect(x, y, x + width, y + height, paint);
-    }
-    
-    /**
-     * Apply the given force to the balloon
-     * @param force
-     */
-    public void applyForce(Force force) {        
-        dx += force.getDx();
-        dy += force.getDy();
-    }
-
+        canvas.drawRect(getX(), getY(), getX() + width, getY() + height, paint);
+    }    
 }
